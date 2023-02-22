17f1efba38a34ea04f4bc391862655a4d85cd92a
Yi Man <yman4@jhu.edu>
2023-02-21 22:43:15 -0500

    Implement linear related MLE

--- R/hiper_glm.R
+++ R/hiper_glm.R
@@ -1,11 +1,19 @@
 #' @export
-hiper_glm <- function(design, outcome, model = "linear"){
+hiper_glm <- function(design, outcome, model = "linear", option = list()){
   supported_model <- c("linear", "logit")
   if(!(model %in% supported_model)){
-    stop(sprintf(""))
+    stop(sprintf("Model not supported yet."))
   }
-  # TODO: find MLE.
-  hglm_out <- list()
+
+  if(model == "linear"){
+    if(is.null(option$mle_solver)){
+      MLE <- linear_mle_pseudo_inv(design, outcome)
+    } else if(option$mle_solver=="BFGS"){
+      MLE <- linear_mle_bfgs(design, outcome)
+    }
+  }
+
+  hglm_out <- list(coef = MLE)
   class(hglm_out) <- "hglm"
   return(hglm_out)
 }
