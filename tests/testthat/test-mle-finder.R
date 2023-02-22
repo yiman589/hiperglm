c8d4eeb35bdb3ae9fd42e5bebe38b37dd09ce601
Yi Man <yman4@jhu.edu>
2023-02-21 22:36:48 -0500

    Added comparing between numerical and analytical test

--- tests/testthat/test-mle-finder.R
+++ tests/testthat/test-mle-finder.R
@@ -10,3 +10,14 @@ test_that("linalg and optim least-sq coincide", {
     coef(via_linalg_out), coef(via_bfgs_out), abs_tol = 1e-2, rel_tol = 1e-2
   ))
 })
+
+test_that("gradient calculation by comparing it against a numerical one", {
+  n_obs <- 32; n_pred <- 4
+  data <- simulate_data(n_obs, n_pred, model = 'linear', seed = 1918)
+  design <- data$design; outcome <- data$outcome
+  via_analytical_out <- linear_gradient(rep(1,n_pred), design, outcome)
+  via_numerical_out <- approx_grad(linear_log_likelihood, rep(1,n_pred), design, outcome)
+  expect_true(are_all_close(
+    via_analytical_out, via_numerical_out, abs_tol = 1e-2, rel_tol = 1e-2
+  ))
+})
