1f5eca8b0889662bc71c6fa2a4c9cb70afb16393
Yi Man <yman4@jhu.edu>
2023-03-03 16:14:33 -0500

    addressing hw2 comments.

--- R/hiper_glm.R
+++ R/hiper_glm.R
@@ -2,16 +2,16 @@
 hiper_glm <- function(design, outcome, model = "linear", option = list()){
   supported_model <- c("linear")
   if(!(model %in% supported_model)){
-    stop(sprintf("Model not supported yet."))
+    stop("Model not supported yet.")
   }
 
   if(model == "linear"){
-    if(is.null(option$mle_solver)){
+    if(is.null(option$mle_solver)||(option$mle_solver == "pseudo-inverse")){
       MLE <- linear_mle_pseudo_inv(design, outcome)
     } else if(option$mle_solver == "BFGS"){
       MLE <- linear_mle_bfgs(design, outcome)
     } else {
-      stop(sprintf("Method not supported yet."))
+      stop("Method not supported yet.")
     }
   }
 
